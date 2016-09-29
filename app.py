1619107220637ad1b257127e3f5bbb7e14d84cb5
Derek Hohls <gamesbook@gmail.com>
2016-09-29 12:32:11 +0200

    Simplify app.py

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ class SocketHandler(websocket.WebSocketHandler):
 
     def on_message(self, message):
         for client in clients:
-            if client != self:
-                client.write_message(message)
+            client.write_message(message)
 
     def on_close(self):
         clients.remove(self)
@@ -34,28 +33,9 @@ class SocketHandler(websocket.WebSocketHandler):
         if self in cl:
             cl.remove(self)
 
-class ApiHandler(web.RequestHandler):
-
-    @web.asynchronous
-    def get(self, *args):
-        self.finish()
-        id = self.get_argument("id")
-        value = self.get_argument("value")
-        data = {"id": id, "value" : value}
-        data = json.dumps(data)
-        for c in cl:
-            c.write_message(data)
-
-    @web.asynchronous
-    def post(self):
-        pass
-
 app = web.Application([
     (r'/', IndexHandler),
     (r'/ws', SocketHandler),
-    (r'/api', ApiHandler),
-    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
 ])
 
 if __name__ == '__main__':
